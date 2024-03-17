7dd91542c36e1bddb1527fd77c765cdfe92cda88
shrookayman <shrookayman617@gmail.com>
2024-03-17 14:34:11 +0200

    image

--- ServicesApp/Dto/Service/PostServiceRequestDto.cs
+++ ServicesApp/Dto/Service/PostServiceRequestDto.cs
@@ -6,7 +6,7 @@
         public required string Description { get; set; }
         public string Location { get; set; }
 		public string PaymentMethod { get; set; }
-		// public byte[]? Image { get; set; }
+	    public byte[]? Image { get; set; }
 		//public int MaxFees { get; set; } = 0;
 	}
 }

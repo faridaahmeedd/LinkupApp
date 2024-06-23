bab98b7d25fc5b689dd0445e8e17b1172396c83e
shrookayman <shrookayman617@gmail.com>
2024-06-23 15:41:17 +0300

    reporterrole

--- ServicesApp/Dto/Reviews_Reports/GetReportDto.cs
+++ ServicesApp/Dto/Reviews_Reports/GetReportDto.cs
@@ -5,6 +5,8 @@
 		public int Id { get; set; } 
 		public string Comment { get; set; }
 		public required string ReporterName { get; set; }
-		public required int RequestId { get; set; }
+        public required string ReporterRole { get; set; }
+
+        public required int RequestId { get; set; }
 	}
 }
